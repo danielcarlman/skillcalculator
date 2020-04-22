0871e6854729afb998ce97c137f3d6db519cb483
danielcarlman <daniel.v.carlman@gmail.com>
2020-04-21 21:11:38 -0600

    Created states and a function to toggle buttons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import * as C from './styles';
 import Button from './Components/Button';
 import Navbar from './Components/Navbar';
 
-function App() {
-  return (
-    <C.Container>
-      <Navbar />
-      <C.Title>Front-End Skill Calculator</C.Title>
-      <Button checked name='OK ✔' />
-      <Button name='Check' />
-    </C.Container>
-  );
+class App extends Component {
+  state = {
+    isChecked: false,
+    html: false,
+    css: false,
+  };
+
+  trocarBotao = (value) => {
+    this.setState({ [value]: !this.state[value] });
+    console.log(value);
+  };
+
+  render() {
+    return (
+      <C.Container>
+        <Navbar />
+        <C.Title>Front-End Skill Calculator</C.Title>
+        <Button
+          toggleButton={this.trocarBotao}
+          checked={this.state.html}
+          value={'html'}
+        />
+        <Button
+          toggleButton={this.trocarBotao}
+          checked={this.state.css}
+          value={'css'}
+        />
+      </C.Container>
+    );
+  }
 }
 
 export default App;
--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import * as C from './styles';
 
 export default function Button(props) {
-  return <C.Button checked={props.checked}>{props.name}</C.Button>;
+  return (
+    <C.Button
+      onClick={() => props.toggleButton(props.value)}
+      checked={props.checked}
+    >
+      {props.checked ? 'OK ✔' : 'Check'}
+    </C.Button>
+  );
 }
