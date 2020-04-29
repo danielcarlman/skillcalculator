63bbc16b456e56655aa5fe230182a76e0c1cbd68
danielcarlman <daniel.v.carlman@gmail.com>
2020-04-29 15:10:14 -0600

    CSS Grid made responsive with auto-fit and minmax

--- src/styles.js
+++ src/styles.js
@@ -20,7 +20,7 @@ const CounterContainer = styled.div`
 `;
 
 const Counter = styled.h1`
-  color: white;
+  color: orchid;
   font-size: 5rem;
   font-weight: 700;
   opacity: 1;
@@ -28,11 +28,11 @@ const Counter = styled.h1`
 
 const BoxContainer = styled.div`
   display: grid;
-  grid-template-rows: repeat(7, 150px);
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(14, 150px);
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
   grid-gap: 30px;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   padding: 30px 30px;
 `;
 
@@ -43,6 +43,7 @@ const Box = styled.div`
   align-items: center;
   background-color: ${(props) => props.color};
   border-radius: 30px;
+  text-align: center;
 `;
 
 export { Container, CounterContainer, Counter, BoxContainer, Box };
