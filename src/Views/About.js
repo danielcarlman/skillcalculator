eff0e7553bcfabe7314c2e9678a0d5e02527c77e
danielcarlman <daniel.v.carlman@gmail.com>
2020-05-27 17:48:16 -0600

    Created skeleton for the About page

--- src/Views/About.js
+++ src/Views/About.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import * as C from '../styles';
+import * as C from './styles';
 
 export default class About extends Component {
   render() {
     return (
       <C.Container>
-        <C.BoxContainer>
-          <C.Box>
-            <C.InfoText>Hi</C.InfoText>
-          </C.Box>
-        </C.BoxContainer>
+        <C.Box>
+          <C.BoxSmallText>
+            My name is Daniel Carlman! I'm a Front-End Developer
+          </C.BoxSmallText>
+        </C.Box>
       </C.Container>
     );
   }
--- src/Views/styles.js
+++ src/Views/styles.js
@@ -4,111 +4,32 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: black;
-  color: white;
-`;
-const CounterContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  top: 0;
   width: 100%;
-  position: ${(props) => (props.isClicked ? 'static' : 'sticky')};
-  background: rgb(0, 0, 0, 0.75);
-  margin-top: 2rem;
-`;
-
-const Counter = styled.h1`
-  color: orchid;
-  font-size: ${(props) => (props.isClicked ? '3rem' : '5rem')};
-  font-weight: 700;
-  opacity: 1;
-  margin: 0;
-`;
-
-const Results = styled.h1`
-  display: ${(props) => (props.isClicked ? 'block' : 'none')};
-  color: orchid;
-  font-size: 3rem;
-  font-weight: 700;
-  opacity: 1;
-  margin: 0;
-`;
-
-const BoxContainer = styled.div`
-  display: grid;
-  grid-auto-rows: ${(props) =>
-    props.isClicked ? '1fr' : 'minmax(250px, 1fr)'};
-  grid-template-columns: ${(props) =>
-    props.isClicked ? '1fr' : 'repeat(auto-fill, minmax(22rem, 1fr))'};
-  grid-gap: 30px;
-  width: 80%;
-  @media screen and (max-width: 600px) {
-    width: 100%;
-  }
-  height: 100%;
-  padding: 30px 50px;
-`;
-
-const BoxText = styled.h2`
-  font-size: 2.5rem;
-  font-weight: 700;
+  color: white;
 `;
 
 const Box = styled.div`
-  display: ${(props) => (props.hidden ? 'none' : 'flex')};
-  flex-direction: column;
-  justify-content: ${(props) => (props.isClicked ? 'center' : 'space-between')};
-
-  @media screen and (max-width: 600px) {
-    justify-content: center;
-  }
-
-  align-items: center;
-  background-color: ${(props) => props.color};
-  border-radius: 30px;
-  text-align: center;
-  padding: 5rem 1rem;
-`;
-
-const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: ${(props) => (props.isClicked ? 'center' : 'space-between')};
   align-items: center;
+  background-color: red;
+  border-radius: 30px;
+  text-align: center;
+  margin: 5rem;
+  padding: 1rem 5rem;
+  width: 80%;
 `;
 
-const InfoText = styled.h2`
-  font-size: 2.5rem;
-  margin: 2rem 2rem;
-
-  @media screen and (max-width: 600px) {
-    font-size: 1.5rem;
-  }
+const BoxText = styled.h2`
+  font-size: 5rem;
+  font-weight: 700;
 `;
 
-const InfoLink = styled.div`
-  a {
-    font-size: 1.5rem;
-    color: white;
-    text-decoration: none;
-
-    &:hover {
-      text-decoration: underline;
-    }
-  }
+const BoxSmallText = styled.h3`
+  font-size: 3rem;
+  font-weight: 700;
+  line-height: 5rem;
 `;
 
-export {
-  Container,
-  CounterContainer,
-  Counter,
-  Results,
-  BoxContainer,
-  BoxText,
-  Box,
-  InfoContainer,
-  InfoText,
-  InfoLink,
-};
+export { Container, BoxText, Box, BoxSmallText };
