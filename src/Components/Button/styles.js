7297d12c1ad49aa7f1c264c37017f45604c9a002
danielcarlman <daniel.v.carlman@gmail.com>
2020-06-11 15:11:53 -0600

    Removed unnecessary CSS code

--- src/Components/Button/styles.js
+++ src/Components/Button/styles.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const Button = styled.button`
   font-family: sans-serif;
   font-size: 1.3rem;
-  border: none;
   border-radius: 2rem;
   padding: 1rem 1.5rem;
   width: 10rem;
